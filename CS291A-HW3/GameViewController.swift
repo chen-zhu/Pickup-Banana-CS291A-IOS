27e48e2eaa522c6bf0b819229af97b13f9bdc6f2
Chen Z <chenzhu@ucsb.edu>
2020-05-09 13:12:27 -0700

    Add a few more bananas

--- CS291A-HW3/GameViewController.swift
+++ CS291A-HW3/GameViewController.swift
@@ -16,8 +16,9 @@ class GameViewController: UIViewController, ARSessionDelegate{
     
     var planAnchor = try! Experience.loadPlan()
     var cameraAnchor = AnchorEntity(.camera)
-    
     var time = Date().timeIntervalSince1970
+    
+    var performance_tuner = 0
         
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +37,7 @@ class GameViewController: UIViewController, ARSessionDelegate{
         //print(planAnchor.children[0].children[1].position)
         //print(planAnchor.banana?.position)
         //print(planAnchor.sign?.position)
-        arView.physicsOrigin = planAnchor.sign
+        //arView.physicsOrigin = planAnchor.sign
         
         self.setupObjNotifyActions(arView: arView)
         
@@ -45,24 +46,25 @@ class GameViewController: UIViewController, ARSessionDelegate{
     
     // MARK: - ARSessionDelegate
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
-        if(self.time + 4 > Date().timeIntervalSince1970){
-            print("Time Buffer detected")
-        } else if(self.cameraAnchor.children.isEmpty == false){
-            let banana = self.cameraAnchor.children[0]
-            let banana_position = banana.position(relativeTo: self.planAnchor)
-            
-            if(banana_position.y <= 0.015){
-                print("touched the plan~")
-                banana.setParent(self.planAnchor, preservingWorldTransform: true)
-                var pos = banana.position
-                pos.y = 0.015
-                banana.position = pos
+        //if(self.performance_tuner > 0){
+            if(self.time + 2 > Date().timeIntervalSince1970){
+                //print("Putting down: Time Buffer detected")
+            } else if(self.cameraAnchor.children.isEmpty == false){
+                let banana = self.cameraAnchor.children[0]
+                let banana_position = banana.position(relativeTo: self.planAnchor)
+                
+                if(banana_position.y <= 0.015){
+                    print("touched the plan~")
+                    banana.setParent(self.planAnchor, preservingWorldTransform: true)
+                    var pos = banana.position
+                    pos.y = 0.015
+                    banana.position = pos
+                }
+                
+                self.time = Date().timeIntervalSince1970
+                self.performance_tuner = 0
             }
-            /*else {
-                print("above the plan~")
-            }*/
-        }
-            
+        //}
         
     }
     
@@ -74,17 +76,26 @@ class GameViewController: UIViewController, ARSessionDelegate{
     // MARK: - Object Notification Action
     func setupObjNotifyActions(arView: ARView){
         self.planAnchor.actions.pickupBanana.onAction = { entity in
-            print("Picking up banana~")
-            self.cameraAnchor = AnchorEntity(.camera)
-            self.cameraAnchor.name = "cameraAnchor"
-            arView.scene.addAnchor(self.cameraAnchor)
-            entity?.setParent(self.cameraAnchor, preservingWorldTransform: true)
-            //var cam_transform = cameraAnchor.transform
-            //cam_transform.translation.z = -0.12
-            //entity?.move(to: cam_transform, relativeTo: cameraAnchor, duration: 1)
-            entity?.position = SIMD3(0, 0, -0.12)
-            //print(cameraAnchor)
-            self.time = Date().timeIntervalSince1970
+            //Only pick up banana if the current camera anchor has nothing
+            //self.cameraAnchor = AnchorEntity(.camera)
+            if(self.performance_tuner <= 0){
+                if(self.time + 2 > Date().timeIntervalSince1970){
+                    //print("Picking up: Time Buffer detected")
+                } else if(self.cameraAnchor.children.isEmpty) {
+                    print("Picking up banana~")
+                    self.cameraAnchor.name = "cameraAnchor"
+                    arView.scene.addAnchor(self.cameraAnchor)
+                    entity?.setParent(self.cameraAnchor, preservingWorldTransform: true)
+                    //var cam_transform = cameraAnchor.transform
+                    //cam_transform.translation.z = -0.12
+                    //entity?.move(to: cam_transform, relativeTo: cameraAnchor, duration: 1)
+                    entity?.position = SIMD3(0, 0, -0.12)
+                    self.time = Date().timeIntervalSince1970
+                    
+                    self.performance_tuner = 1;
+                    print(self.cameraAnchor.children)
+                }
+            }
         }
     }
     
